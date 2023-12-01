8f1165d2f7216e39ad5ce9680a5c8d024ba7fe20
srihari-akurathi <135617586+srihari-akurathi@users.noreply.github.com>
2023-12-01 19:43:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,5 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:
     back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+    streamlit.dataframe(back_from_function);
+
