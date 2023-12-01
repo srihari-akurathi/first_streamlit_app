02e09c729b91ecfb5e166d718cd1e0073e759240
srihari-akurathi <135617586+srihari-akurathi@users.noreply.github.com>
2023-12-01 19:24:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,10 @@ try:
     back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function);
 
+
+streamlit.stop();
+
+
 streamlit.stop();
 
 #import snowflake.connector;
