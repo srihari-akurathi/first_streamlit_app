df8a2926242dbcdfdbdd76e415a16abad985cdc6
srihari-akurathi <135617586+srihari-akurathi@users.noreply.github.com>
2023-12-01 19:23:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,9 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:
     back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+    streamlit.dataframe(back_from_function);
 
-streamlit.stop()
+streamlit.stop();
 
 #import snowflake.connector;
 
