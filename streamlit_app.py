4e76b46267bb24ed50550f15c148e80aaf232586
srihari-akurathi <135617586+srihari-akurathi@users.noreply.github.com>
2023-12-01 20:55:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,19 +27,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-def get_fruityvice_data (this_fruit_choice):
-  fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  return fruityvice_normalized;
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
   
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    back_from_function=get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
+   fruit_choice = streamlit.text_input('What fruit would you like information about?)
+   if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+   else:
+       back_from_function=get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
 
 #import snowflake.connector
 def get_fruit_load_list():
